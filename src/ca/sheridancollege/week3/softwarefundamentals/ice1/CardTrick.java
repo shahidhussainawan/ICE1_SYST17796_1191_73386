06b4263563ffa63a6c98e20493d1e8ecdc1cb3b3
rileylgray <grayril@sheridancollege.ca>
2019-02-01 17:26:01 -0500

    add name and date

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifer Riley Gray 2019-02-01
  */
 public class CardTrick {
     
