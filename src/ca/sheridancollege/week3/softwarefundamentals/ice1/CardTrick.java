306baef89f33e69e479761dfc1ee098d5ce7070a
Shaimaa Ali-sheridan <shaimaa.alil@sheridancollege.ca>
2019-02-08 14:20:16 -0500

    first commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +22,25 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+     Scanner card1= new Scanner(System.in);
+     System.out.println("engter number and suit");
+     int number = card1.nextInt();
+     String suits = card1.next();
+     c.setValue(number);
+     c.setSuit(suits);
+     System.out.println("number is "+c.getValue() +"SUIT IS:"+c.getSuit());
+     magicHand[i] = c;
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
-    
+     System.out.println("enter card to be searched");
+     Scanner card2 = new Scanner(System.in);
+     Card[] card12 = new Card[1];
+     int value= card2.nextInt();
+     String s = card2.next();
+     if(card12==magicHand){
+         System.out.println("card exists");
+     }
+     else{
+         System.out.println("doenot exist");
+     }
+}
 }
