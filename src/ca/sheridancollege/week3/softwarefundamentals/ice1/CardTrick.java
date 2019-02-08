0e08a8180e1d8ee35636a947245d47e88df77731
gundeepsandhu <47257346+gundeepsandhu@users.noreply.github.com>
2019-02-08 14:57:52 -0500

    Add Lucky Card Step

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author shaimaa ali
+ * @author Shaimaa Ali
  * @modifiedby Gundeep Singh , 991530217
  * 
  */
@@ -52,6 +52,13 @@ public class CardTrick {
             }
         }
            
+        Card luckyCard=new Card();
+            luckyCard.setValue(9);
+            luckyCard.setSuit("Diamonds");
+            System.out.println("Lucky card is: ");
+            System.out.println(luckyCard.getValue());
+            System.out.println(luckyCard.getSuit());
+        
             
     }
     
